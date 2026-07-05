0b06c67c8ebb55b870dfd12f33ca344b8d7dfe55
Juan Beresiarte <juanberesiarte@gmail.com>
2026-07-05 00:11:44 -0300

    feat(config): configure Astro with Tailwind, MDX, sitemap and Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+import mdx from "@astrojs/mdx";
+
+import vercel from "@astrojs/vercel";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://blog.beresiarte.xyz",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [mdx(), sitemap()],
+  adapter: vercel(),
+});
